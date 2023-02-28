92550f5ff452e8b04d96a3ff8561a072ad930c33
martoast <alexmartos96@gmail.com>
2023-02-28 15:33:38 -0800

    try it

--- components/About.vue
+++ components/About.vue
@@ -6,7 +6,9 @@
             <img src="~/assets/img/placeholder.png" class="d-block mx-lg-auto img-fluid" alt="Bootstrap Themes" width="700" height="500" loading="lazy">
         </div>
         <div class="col-lg-6">
-            <h1 class="display-5 fw-bold lh-1 mb-3">Bring a project to life before it's even built</h1>
+            <h1 class="display-5 fw-bold lh-1 mb-3">
+                Bring a project to life before it's even built
+            </h1>
             <p class="lead">Explore immersive 3D renders that bring your future real estate projects to life before it's even built, all from the comfort of your device. Whether you're a home buyer or real estate professional, RealTourCo's state-of-the-art virtual tours and VR-compatible technology make it easy to showcase and envision properties like never before.</p>
         </div>
         </div>
