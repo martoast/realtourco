eeef9afd5e2977c6f970d683180e5d5d31a3d29a
martoast <alexmartos96@gmail.com>
2023-02-28 22:33:32 -0800

    about changes

--- components/Projects.vue
+++ components/Projects.vue
@@ -1,6 +1,6 @@
 <template>
     <div class="container">
-        <h2>Featured Projects</h2>
+        <h2>Showcase</h2>
         <div class="row row-cols-1 row-cols-lg-3 align-items-stretch g-4 py-5">
             <div class="col">
                 <a href="//arquitectostijuana.shapespark.com/casa_cuatro_veranos/" target="_blank" class="card card-cover h-100 overflow-hidden text-white bg-dark rounded-5 shadow-lg" style="background-image: url('/img/cover.jpeg'); background-size: cover; text-decoration: none;">
